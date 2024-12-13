d8519f17e5ef4a4ed5e424e0ceb9af479767eda6
KateGaga <kate10111204@gmail.com>
2024-12-13 14:52:49 +0800

    日誌記帳頁面

--- app/src/main/java/com/example/daily/Transaction.java
+++ app/src/main/java/com/example/daily/Transaction.java
@@ -1,2 +1,33 @@
-package com.example.daily;public class Transaction {
+package com.example.daily;
+public class Transaction {
+    private String title;
+    private double amount;
+    private String date;
+
+        // Getters and setters
+    public String getTitle() {
+            return title;
+        }
+
+    public void setTitle(String title) {
+            this.title = title;
+        }
+
+    public double getAmount() {
+            return amount;
+        }
+
+    public void setAmount(double amount) {
+            this.amount = amount;
+        }
+
+    public String getDate() {
+            return date;
+        }
+
+    public void setDate(String date) {
+            this.date = date;
+        }
 }
+
+
